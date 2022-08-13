0ac22af44f347462718e601c8bc7c701a79580cd
Utku Güneş <79314852+utkugunes@users.noreply.github.com>
2022-08-13 18:30:02 +0300

    first commit

---
+++ WebAPI/WebAPI/Program.cs
@@ -0,0 +1,45 @@
+using Microsoft.Extensions.FileProviders;
+using Newtonsoft.Json.Serialization;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(c =>
+{
+    c.AddPolicy("AllowOrigin", options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+});
+
+//JSON serializer
+builder.Services.AddControllersWithViews().AddNewtonsoftJson(options=>
+options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore)
+    .AddNewtonsoftJson(options=>options.SerializerSettings.ContractResolver = new DefaultContractResolver() );
+
+
+var app = builder.Build();
+
+app.UseCors(options=>options.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(),"Photos")),
+    RequestPath="/Photo"
+});
